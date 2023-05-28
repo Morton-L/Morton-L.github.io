f599d096a1a683488e7a9a2b18118911dd4ed87d
Morton Li <Morton.L@Outlook.com>
2023-05-28 23:26:57 +0800

    github代理适配CloudFlare

--- static/js/mirrors/github.com.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('accelerate-form').addEventListener('submit', function(e) {
-    e.preventDefault();
-    var githubUrl = document.getElementById('github-url').value;
-    window.open(location.href.substr(0, location.href.lastIndexOf('/') + 1) + githubUrl);
-});
